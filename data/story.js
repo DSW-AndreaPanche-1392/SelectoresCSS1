20cef5bee22af49664becfc9b61d4b75ef0d5b3c
DSW-AndreaPanche-1392 <apanchet@gmail.com>
2023-04-26 23:17:18 -0500

    SelectoresCSS1
    OK

---
+++ data/story.js
@@ -0,0 +1,121 @@
+var story = {
+ "docName": "Selectores CSS",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fontSizeFormat": -1,
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "zoomEnabled": true,
+ "title": "Demo",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "5:14",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Selectores de CSS",
+   "image": "selectores-de-css.png",
+   "imageFull": "full/selectores-de-css.png",
+   "index": 0,
+   "width": 1596,
+   "height": 6234,
+   "x": -688,
+   "y": -336,
+   "type": "regular",
+   "fixedPanels": [
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": 0,
+     "y": 0,
+     "width": 1596,
+     "height": 87,
+     "type": "float",
+     "index": 0,
+     "isFloat": true,
+     "isVertScroll": false,
+     "links": [
+      {
+       "rect": {
+        "x": 1238,
+        "y": 43,
+        "width": 106,
+        "height": 23
+       },
+       "index": 0,
+       "page": 1
+      }
+     ],
+     "image": "selectores-de-css-0.png",
+     "mskH": 87
+    }
+   ],
+   "links": [],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  },
+  {
+   "id": "85:2",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "Acerca de",
+   "image": "acerca-de.png",
+   "imageFull": "acerca-de.png",
+   "index": 1,
+   "width": 1638,
+   "height": 683,
+   "x": 1195,
+   "y": -399,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 899,
+      "y": 43,
+      "width": 58,
+      "height": 23
+     },
+     "index": 1,
+     "page": 0
+    },
+    {
+     "rect": {
+      "x": 1238,
+      "y": 43,
+      "width": 106,
+      "height": 23
+     },
+     "index": 2,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayOutsideClickIgnore": true,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "0:1",
+   "name": "Page 1"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 2,
+ "backColor": "#FFFFFF"
+}
